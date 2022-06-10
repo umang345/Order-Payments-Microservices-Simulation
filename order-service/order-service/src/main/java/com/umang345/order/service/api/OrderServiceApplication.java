efd581ab26a1f7b5c249c20f7b702f3882f3ee48
Umang <umangagarwal345@gmail.com>
2022-06-10 22:35:41 +0530

    Microservice setup : Order Service

---
+++ order-service/order-service/src/main/java/com/umang345/order/service/api/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.umang345.order.service.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
---
+++ order-service/order-service/src/test/java/com/umang345/order/service/api/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umang345.order.service.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
