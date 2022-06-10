9aa80c3106588676913a828953e68f285177fc3d
Umang <umangagarwal345@gmail.com>
2022-06-10 22:38:47 +0530

    Microservice setup : Payment Service

---
+++ payment-service/payment-service/src/test/java/com/umang345/payment/service/api/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umang345.payment.service.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
