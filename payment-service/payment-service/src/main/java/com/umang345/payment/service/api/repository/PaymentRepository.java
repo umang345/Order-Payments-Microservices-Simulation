141e368d11c885b0821865be332f295504b14624
Umang <umangagarwal345@gmail.com>
2022-06-10 23:04:41 +0530

    Added Payment Entity and Payment Repository interface in Payment-Service Microservice

---
+++ payment-service/payment-service/src/main/java/com/umang345/payment/service/api/repository/PaymentRepository.java
@@ -0,0 +1,11 @@
+package com.umang345.payment.service.api.repository;
+
+import com.umang345.payment.service.api.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment,Integer>
+{
+
+}
