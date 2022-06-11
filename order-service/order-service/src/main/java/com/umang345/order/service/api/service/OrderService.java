c5332ac62cbd044806cf599846e2ec0b534aeabd
Umang <umangagarwal345@gmail.com>
2022-06-11 14:23:37 +0530

    Added config server as eureka client

--- order-service/order-service/src/main/java/com/umang345/order/service/api/service/OrderService.java
+++ order-service/order-service/src/main/java/com/umang345/order/service/api/service/OrderService.java
@@ -6,17 +6,25 @@ import com.umang345.order.service.api.common.TransactionResponse;
 import com.umang345.order.service.api.entity.Order;
 import com.umang345.order.service.api.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class OrderService
 {
     @Autowired
     private OrderRepository repository;
 
     @Autowired
+    @Lazy
     private RestTemplate restTemplate;
+
+    @Value("${microservice.payment-service.endpoints.endpoint.uri}")
+    private String ENDPOINT_URL;
 
     public TransactionResponse saveOrder(TransactionRequest request)
     {
@@ -26,7 +34,7 @@ public class OrderService
         payment.setAmount(order.getPrice()*order.getQty());
 
         Payment paymentResponse =
-                restTemplate.postForObject("http://PAYMENT-SERVICE/payment/doPayment",payment,Payment.class);
+                restTemplate.postForObject(ENDPOINT_URL,payment,Payment.class);
 
         String response = "";
 
