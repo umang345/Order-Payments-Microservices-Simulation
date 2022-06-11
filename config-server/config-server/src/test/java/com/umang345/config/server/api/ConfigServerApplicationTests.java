b6ae3376f61315040d83f7bb6bbf25a7e4ba0b07
Umang <umangagarwal345@gmail.com>
2022-06-11 13:23:42 +0530

    Microservice setup : Cloud Config Server

---
+++ config-server/config-server/src/test/java/com/umang345/config/server/api/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umang345.config.server.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
